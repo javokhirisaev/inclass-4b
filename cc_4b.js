8fc6d688923f69adc6cdc70c184ba8a795ae7287
Javokhir Isaev <javokhirisaev@usf.edu>
2025-09-17 19:54:48 -0400

    I defined an array of 5 different products

---
+++ cc_4b.js
@@ -0,0 +1,9 @@
+// Step 2: Defining array of 5 products
+let products = [
+  { sku: "E1001", name: "Laptop", category: "electronics", price: 1000, inventory: 5 },
+  { sku: "C2001", name: "T-Shirt", category: "clothes", price: 25, inventory: 10 },
+  { sku: "G3001", name: "Bananas", category: "groceries", price: 1.5, inventory: 20 },
+  { sku: "H4001", name: "Detergent", category: "household", price: 12, inventory: 8 },
+  { sku: "B5001", name: "Book", category: "other", price: 15, inventory: 6 }
+];
+
